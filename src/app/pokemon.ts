c3f5e51b14cd0f7ca6369e9e32cfc17e9e149607
Wallace <wallace.silva35@fatec.sp.gov.br>
2023-05-02 15:21:55 -0300

    comitacao

--- src/app/pokemon.ts
+++
@@ -1,4 +0,0 @@
-export interface Pokemon{
-  message : string;
-  status : string;
-}
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,20 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { Pokemon } from '../pokemon';
+
+ import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
-  styleUrls: ['./pokemon.component.css']
+  styleUrls: ['./pokemon.component.css'],
 })
 export class PokemonComponent implements OnInit {
-  pokemon : Pokemon = {} as Pokemon;
+  pokemon : any;
+  pokeId: number = 1;
+
+  constructor(private http: HttpClient){}
+
 ngOnInit(): void {
-this.pokemon.message = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png";
+this.loadPokemon;
 }
 
-getBreed(): string[]{
-return this.pokemon.message.split("/")[4].split("-");
+loadPokemon(): void{
 
+  this.http.get(`https://pokeapi.co/api/v2/pokemon/${this.pokeId}`).subscribe(
+    (response) => {
+      this.pokemon = response;
+    },
+    (error) =>{
+      console.log(error);
+    }
+    );
+}
+
+aumentaPoke(): void{
+this.pokeId++;
+if(this.pokeId >1008){
+  this.pokeId = 1;
+}
+this.loadPokemon();
+}
+
+diminuiPoke(): void{
+  this.pokeId--;
+  if(this.pokeId <=0){
+    this.pokeId = 1008;
+  }
+  this.loadPokemon();
+  }
+pokeTipo(): string{
+
+  return this.pokemon.types[0].type.name;
+}
+
+pokeImg(): string{
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${this.pokemon.id}.png`;
 }
 
 }
